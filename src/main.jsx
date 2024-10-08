58f9393246751a968c92e619c66d5573ca8ef1d3
nasimuk47 <nasim.uk47@gmail.com>
2024-10-09 01:57:44 +0530

    single productDetails page complete

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home/Home";
 import App from "./App";
+import SingleProduct from "./pages/Home/SingleProduct";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/shop/:id",
+        element: <SingleProduct></SingleProduct>,
       },
     ],
   },
