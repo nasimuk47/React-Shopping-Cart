c0125a83a45bc2cbddf73ad7342b203c2295af72
nasimuk47 <nasim.uk47@gmail.com>
2024-10-09 01:32:58 +0530

    completd completed category and bestsellers section

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { FaShoppingBag } from "react-icons/fa";
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 import Banner from "./Banner";
+import BestSellers from "./BestSellers";
+import Category from "./Category";
+import Collections from "./Collections";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Category></Category>
+      <Collections></Collections>
+      <BestSellers></BestSellers>
     </div>
   );
 };
